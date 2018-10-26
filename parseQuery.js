5a180ab59779cdec99d0f7134ec146a3e03941f1
loadingmore <biyuqiwan@163.com>
2018-10-26 10:15:27 +0800

    add parseQuery

--- parseQuery.js
+++ parseQuery.js
@@ -1,16 +1,16 @@
 const str = `https://www.baidu.com?charge_id=1&chStation_id=1027&otherMsg=消息`
 
-const urlParse = (url = window.location.href) => {
+const parseQuery = (url = window.location.href) => {
   const path = url.split('?')[1]
   const reg = /(\w+)=([^&]+)/g
   const res = {}
   let m
   if (path && typeof path === 'string') {
     while (m = reg.exec(path)) {
-      res[m[1]] = m[2]
+      res[m[1]] = decodeURIComponent(m[2])
     }
     return res
   }
-  return Throw ('Unusable character')
+  return res
 }
-console.log(urlParse(str))
+console.log(parseQuery(str))
